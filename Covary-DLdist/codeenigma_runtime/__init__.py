540a5f06af2c247dfd6f566721dd682dbfd62f17
mahvin92 <170157865+mahvin92@users.noreply.github.com>
2025-12-24 03:15:24 +0800

    Delete Covary-DLdist directory

--- Covary-DLdist/codeenigma_runtime/__init__.py
+++
@@ -1,9 +0,0 @@
-import platform
-import sys
-import rich
-
-if platform.system() != 'Linux':
-    rich.print("codeenigma_runtime was compiled for 'Linux'. You are on a different OS.")
-    sys.exit(1)
-
-from .codeenigma_runtime import execute_secure_code
